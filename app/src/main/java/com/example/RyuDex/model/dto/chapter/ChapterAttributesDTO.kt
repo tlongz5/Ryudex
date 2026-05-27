070d5739db829fb2281b4c5175d33069c540c069
Thanh Long <btlong2610@gmail.com>
2026-05-27 22:57:10 +0700

    update images

--- app/src/main/java/com/example/RyuDex/model/dto/chapter/ChapterAttributesDTO.kt
+++ app/src/main/java/com/example/RyuDex/model/dto/chapter/ChapterAttributesDTO.kt
@@ -1,7 +1,13 @@
 package com.example.RyuDex.model.dto.chapter
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class ChapterAttributesDTO(
     val chapter: String?,
     val title: String?,
     val translatedLanguage: String?,
-)
+    val pages: Int?,
+    val createdAt: String?
+): Parcelable
